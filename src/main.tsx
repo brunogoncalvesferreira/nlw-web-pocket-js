476f93ea750f623a71c640c3e9646d7a20c11ab6
brunogoncalvesferreira <brunogoncalvesferreira@outlook.com>
2025-05-15 21:17:06 -0300

    commit backend code

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </QueryClientProvider>
+	<QueryClientProvider client={queryClient}>
+		<StrictMode>
+			<App />
+		</StrictMode>
+	</QueryClientProvider>,
 );
